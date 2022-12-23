319de2320fdf9676aad10b12cae350a51863cd0d
unknown <argenis.gonzalez@itksquare.edu.mx>
2022-12-23 08:28:06 -0400

    Render Move done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import './App.css';
-import { useState, handleOnClick } from 'react';
+import { useState, handleOnClick, useEffect } from 'react';
+import History from "./components/History";
 
-function determineWinner (board) {
-    const winningPositions = [
-        [0,1,2],
-        [3,4,5],
-        [6,7,8],
-        [0,3,6],
-        [1,4,7],
-        [2,5,8],
-        [0,4,8],
-        [2,4,6]
-      ]
+function determineWinner(board) {
+  const winningPositions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
 
-    for (let i = 0; i < winningPositions.length; i++){
-      const [a,b,c] = winningPositions[i];
-      
-      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+  for (let e = 0; e < winningPositions.length; e++) {
+    const [a, b, c] = winningPositions[e];
+
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
       return true;
     }
   }
@@ -25,53 +26,79 @@ function determineWinner (board) {
 }
 
 function App() {
-  const [gameState, setGameState] = useState(new Array(9).fill(null))
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [board, setBoard] = useState(history[history.length - 1]);
   const [isXNext, setIsXNext] = useState(true);
-  const winner = determineWinner(gameState);
+  const winner = determineWinner(board);
 
-  const handleOnClick = (i) =>{
-    const newBoard = [...gameState];
-    if(newBoard[i] || winner) return;
-    newBoard[i] = isXNext ? 'X': "O";
-    setIsXNext((prev) => !prev)
-    setGameState(newBoard)
+  useEffect(() => {
+    setBoard(history[history.length - 1]);
+  }, [history]);
+
+  const handleOnClick = (index) => {
+    const copiedHistory = [...history];
+    const copiedBoard = [...board];
+    if (copiedBoard[index] || winner) return;
+    copiedBoard[index] = isXNext ? "X" : "O";
+    setIsXNext(!isXNext);
+    setHistory([...copiedHistory, copiedBoard]);
   }
 
+  const handleReset = () => {
+    setHistory([new Array(9).fill(null)]);
+    setIsXNext(true);
+  };
 
-  const list = gameState.map((value, index) => <Tile value={value} idx={index} key={index} handleOnClick={handleOnClick} />)
-  console.log(list)
-return (
+  const handleChangeBoard = (board) => {
+    setBoard(board);
+  };
+
+  
+  const list = board.map((value, index) => <Tile value={value} idx={index} key={index} handleOnClick={handleOnClick} />)
+  return (
     <div className="App">
-      <Title winner={winner} turn= {isXNext ? 'O':'X'}/>
-        <main className='Board'>
-          
-          {list}
-        </main>
+      <Title winner={winner} turn={isXNext ? 'O' : 'X'} />
+      <main className='Board'>
+        {list}
+      </main>
+      <Historial />
+      <History moves={history} handleTimeTravel={handleChangeBoard} />
+      {/* <button onClick={handleReset}>Play Again</button> */}  
     </div>
   );
 }
 
 
-function Tile ({value, handleOnClick, idx}) { 
+function Tile({ value, handleOnClick, idx }) {
+  console.log(value)
 
-  return( 
-     <button className= 'Tile' onClick={() => {
+  return (
+    <button className='Tile' onClick={() => {
       handleOnClick(idx);
-     }}>
-        {value}
-     </button>
-     )
+    }}>
+      {value}
+    </button>
+  )
 }
 
 
 function Title({ winner, turn }) {
-  console.log(winner, turn)
-   return(
-     <h5>
-       {winner ? 'Winner: ': 'Tic Tac Toe'} {winner && turn}
+  return (
+    <h5 className='Title'>
+      {winner ? 'Winner: ' : 'Tic Tac Toe'} {winner && turn}
     </h5>
-    )
-} 
+  )
+}
+
+function Historial() {
+ /*  console.log('props', props) */
+  return (
+    <div className='Historial'>
+       {"Game Historial"} 
+    </div>
+  )
+
+}
 
 export default App;
 
